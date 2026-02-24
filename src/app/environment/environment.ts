1892f639ac8ac8df3f42a1326bf2fd428c479d58
Yilsonjr <ing.jimrod@gmail.com>
2026-02-24 13:19:18 -0400

    actualizacion de db

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    SUPABASE_URL: 'https://bspjfskjyrobrboquidm.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzcGpmc2tqeXJvYnJib3F1aWRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NDc1MjEsImV4cCI6MjA4NzIyMzUyMX0.7nKWy4lyuoQ2wQQRpC4lz5bxVhvuT_FUka_UIbIfc0c'
+    SUPABASE_URL: 'https://tisldfszcdahxqmsajfm.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpc2xkZnN6Y2RhaHhxbXNhamZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NDcxMzUsImV4cCI6MjA4NzUyMzEzNX0.WjZypRspvUHKBazO83uaMDGRowlVoVgIUGDR7yWs9oc'
 };
