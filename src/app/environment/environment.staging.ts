c570dd9b4b38d42735b09ec88d8b94a0308a8293
Yilsonjr <ing.jimrod@gmail.com>
2026-05-10 20:15:51 -0400

    feat: configuracion staging restaurante

---
+++ src/app/environment/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    SUPABASE_URL: 'https://tisldfszcdahxqmsajfm.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpc2xkZnN6Y2RhaHhxbXNhamZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NDcxMzUsImV4cCI6MjA4NzUyMzEzNX0.WjZypRspvUHKBazO83uaMDGRowlVoVgIUGDR7yWs9oc'
+};
